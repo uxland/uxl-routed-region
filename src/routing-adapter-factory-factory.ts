9c1f153489208e23d76fbf096a3d38b74ac736ea
Alex Vizcaino <avizcaino@uxland.es>
2019-10-01 15:55:26 +0200

    fix: test if host has 'router-region' attribute

--- src/routing-adapter-factory-factory.ts
+++ src/routing-adapter-factory-factory.ts
@@ -1,14 +1,15 @@
-import {RouterRegionDefinition} from "./router-region-decorator";
-import {IRegionHost, DynamicFactory} from "@uxland/uxl-regions";
-import {RoutingAdapter} from "./routing-adapter";
-import {Router} from "@uxland/uxl-routing";
-import {Store} from 'redux';
+import { DynamicFactory, IRegionHost } from '@uxland/uxl-regions';
+import { Router } from '@uxland/uxl-routing';
+import { Store } from 'redux';
+import { RouterRegionDefinition } from './router-region-decorator';
+import { RoutingAdapter } from './routing-adapter';
 
-export const routingAdapterFactoryFactory = (router: Router, store: Store<any, any>) =>
-    (definition: RouterRegionDefinition, host: Element & IRegionHost) =>
-        new RoutingAdapter(host, router, store, definition);
+export const routingAdapterFactoryFactory = (router: Router, store: Store<any, any>) => (
+  definition: RouterRegionDefinition,
+  host: Element & IRegionHost
+) => new RoutingAdapter(host, router, store, definition);
 
-export const routingAdapterFactory: (router: Router, store: Store<any, any>) => DynamicFactory = (router, store) =>{
-    const adapterFactory = routingAdapterFactoryFactory(router, store);
-    return host => host['router-region'] ? adapterFactory : undefined;
+export const routingAdapterFactory: (router: Router, store: Store<any, any>) => DynamicFactory = (router, store) => {
+  const adapterFactory = routingAdapterFactoryFactory(router, store);
+  return host => (host['router-region'] || host.hasAttribute('router-region') ? adapterFactory : undefined);
 };
