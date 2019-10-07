850bc915faf60795209be08f520087c190764e08
Alex Vizcaino <avizcaino@uxland.es>
2019-10-07 11:50:57 +0200

    demo

--- demo/demo-app.ts
+++ demo/demo-app.ts
@@ -8,15 +8,15 @@ import {
   routingMixin,
   routingSelectors
 } from '@uxland/uxl-routing';
+import { propertiesObserver } from '@uxland/uxl-utilities';
 import { customElement, html, LitElement, property } from 'lit-element';
 import * as redux from 'redux';
-import { compose, createStore } from 'redux';
 import { routerRegion } from '../src/router-region-decorator';
 import { RoutedViewDefinition } from '../src/routing-adapter';
 import { routingAdapterFactoryFactory } from '../src/routing-adapter-factory-factory';
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(redux.combineReducers({ routing: reducer }), composeEnhancers());
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose;
+const store = redux.createStore(redux.combineReducers({ routing: reducer }), composeEnhancers());
 // const store = redux.createStore(redux.combineReducers({ routing: reducer }));
 const Routing = routingMixin(store, routingSelectors);
 const router = new Router(store.dispatch, window.location.href);
@@ -56,15 +56,15 @@ regionManager.registerViewWithRegion<RoutedViewDefinition>('router12', 'item-det
 });
 
 @customElement('demo-app')
-export class DemoApp extends RegionHost(Routing(LitElement)) {
+export class DemoApp extends RegionHost(Routing(propertiesObserver(LitElement))) {
   @property()
   route: string;
 
   render() {
     return html`
       <div>
-        <a href="view1">View1</a>
-        <a href="view2">View2</a>
+        <a href="./view1">View1</a>
+        <a href="./view2">View2</a>
       </div>
       <iron-pages id="routed-region"></iron-pages>
     `;
@@ -76,10 +76,10 @@ export class DemoApp extends RegionHost(Routing(LitElement)) {
   @routerRegion({ name: 'router1', targetId: 'routed-region', route: '' })
   region: IRegion;
   routeChanged(current: Route, previous: Route) {
-    console.log('Route changed: ', current, previous);
+    console.log('Demo - Route changed: ', current, previous);
   }
   isRouteActiveChanged(current: boolean, previous: boolean): void {
-    console.log('Active changed:', current, previous);
+    console.log('Demo - Active changed:', current, previous);
   }
 
   paramsChanged(current: Object, previous: object) {}
@@ -89,13 +89,13 @@ export class DemoApp extends RegionHost(Routing(LitElement)) {
   pageChanged(current: string, previous: string) {}
 }
 @customElement('module-a')
-export class ModuleA extends RegionHost(Routing(LitElement)) {
+export class ModuleA extends RegionHost(Routing(propertiesObserver(LitElement))) {
   render() {
     return html`
       <h1>Module A</h1>
       <div>
-        <a href="view1/view1">View1.1</a>
-        <a href="view1/view2">View1.2</a>
+        <a href="./view1/view1">View1.1</a>
+        <a href="./view1/view2">View1.2</a>
       </div>
       <iron-pages id="routed-region"></iron-pages>
     `;
@@ -103,10 +103,10 @@ export class ModuleA extends RegionHost(Routing(LitElement)) {
   @routerRegion({ name: 'router11', targetId: 'routed-region', route: 'view1' })
   region: IRegion;
   routeChanged(current: Route, previous: Route) {
-    console.log('Route changed: ', current, previous);
+    console.log('Module A - Route changed: ', current, previous);
   }
   isRouteActiveChanged(current: boolean, previous: boolean): void {
-    console.log('Active changed:', current, previous);
+    console.log('Module A - Active changed:', current, previous);
   }
 
   paramsChanged(current: Object, previous: object) {}
@@ -116,13 +116,13 @@ export class ModuleA extends RegionHost(Routing(LitElement)) {
   pageChanged(current: string, previous: string) {}
 }
 @customElement('module-b')
-export class ModuleB extends RegionHost(Routing(LitElement)) {
+export class ModuleB extends RegionHost(Routing(propertiesObserver(LitElement))) {
   render() {
     return html`
       <h1>Module B</h1>
       <div>
-        <a href="view2/view1">View2.1</a>
-        <a href="view2/view2">View2.2</a>
+        <a href="./view2/view1">View2.1</a>
+        <a href="./view2/view2">View2.2</a>
       </div>
       <iron-pages id="routed-region" route="view2"></iron-pages>
     `;
@@ -130,10 +130,10 @@ export class ModuleB extends RegionHost(Routing(LitElement)) {
   @routerRegion({ name: 'router12', targetId: 'routed-region', route: 'view2' })
   region: IRegion;
   routeChanged(current: Route, previous: Route) {
-    console.log('Route changed: ', current, previous);
+    console.log('Module B - Route changed: ', current, previous);
   }
   isRouteActiveChanged(current: boolean, previous: boolean): void {
-    console.log('Active changed:', current, previous);
+    console.log('Module B - Active changed:', current, previous);
   }
 
   paramsChanged(current: Object, previous: object) {}
@@ -147,9 +147,9 @@ export class ItemsList extends LitElement {
   render() {
     return html`
       <ul>
-        <li><a href="view2/view2/0001?desc=hello">Item1</a></li>
-        <li><a href="view2/view2/0002?desc=good">Item2</a></li>
-        <li><a href="view2/view2/0003?desc=bye">Item3</a></li>
+        <li><a href="./view2/view2/0001?desc=hello">Item1</a></li>
+        <li><a href="./view2/view2/0002?desc=good">Item2</a></li>
+        <li><a href="./view2/view2/0003?desc=bye">Item3</a></li>
       </ul>
     `;
   }
@@ -165,7 +165,7 @@ export class ItemDetail extends Routing(LitElement) {
   render() {
     return html`
       <div>
-        <span>${(<any>this).params.id}</span>
+        <span>${this.params && (<any>this).params.id}</span>
         <h1>${(<any>this).query}</h1>
       </div>
     `;
